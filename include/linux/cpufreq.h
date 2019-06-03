7b8362ef02947b41d4f473fd17d7908416413b54
Victor Bo <bvoid@yandex.ru>
2019-06-03 05:19:05 +0000

    cpufreq: add xperience
     	modified:   drivers/cpufreq/Kconfig
     	modified:   drivers/cpufreq/Makefile
     	new file:   drivers/cpufreq/cpufreq_xperience.c
     	modified:   include/linux/cpufreq.h
    
    # Conflicts:
    #	arch/arm64/configs/ugglite_defconfig
    #	drivers/cpufreq/Kconfig
    #	drivers/cpufreq/Makefile
    #	include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -525,6 +525,9 @@ extern struct cpufreq_governor cpufreq_gov_blu_active;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDPLUS)
 extern struct cpufreq_governor cpufreq_gov_ondemandplus;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_ondemandplus)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_XPERIENCE)
+extern struct cpufreq_governor cpufreq_gov_xperience;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_xperience)
 #endif
 
 /*********************************************************************
